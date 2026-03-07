b8241ede240565e18a7575653f5ce50e09fb03d2
ball-castle <bin33138@gmail.com>
2026-03-07 21:28:36 +0800

    1

--- binary_lifting.cpp
+++ binary_lifting.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 using ll = long long;
 
+ll logn[10005], f[17][10005];
+
+// ST list sparse table : idempoten
+// x opt x = x, (a opt b) opt c = a opt (b opt c)
+// gcd, max
+void pre() {
+    logn[2] = 1;
+    for(ll i = 3; i < 1e4 + 5; i++) logn[i] = log[i / 2] + 1;
+}
+
+
 int main() {
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    pre();
+
+    ll n, m;
+    cin >> n >> m;
+    for(ll i = 1; i <= n; i++) cin >> f[0][i];
+    for(ll j = 1; j  )
 }
